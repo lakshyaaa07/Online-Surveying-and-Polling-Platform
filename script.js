a51e22457f28dd94e32067c2c375a570d7bb9b5c
Keshav <keshavm9978@gmail.com>
2023-09-23 06:22:08 +0530

    Did a lot of things

---
+++ script.js
@@ -0,0 +1,20 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    const themeToggle = document.getElementById('bd-theme');
+    const themeText = document.getElementById('bd-theme-text');
+
+    themeToggle.addEventListener('click', function() {
+        const currentTheme = document.documentElement.getAttribute('data-bs-theme');
+        const newTheme = (currentTheme === 'dark') ? 'light' : 'dark';
+
+        document.documentElement.setAttribute('data-bs-theme', newTheme);
+        localStorage.setItem('theme', newTheme);
+        themeText.textContent = `Toggle theme (${newTheme})`;
+    });
+
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+        document.documentElement.setAttribute('data-bs-theme', savedTheme);
+        themeText.textContent = `Toggle theme (${savedTheme})`;
+    }
+});
